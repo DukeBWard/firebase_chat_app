312c54d1d42cedbe73ad24be728ab8491dbae57c
Luke Ward <lbw4459@rit.edu>
2024-03-20 23:08:45 -0400

    adding more components and going to bed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+        <div className='searchForm'>
+            <input type="text" placeholder='Find a user'/>
+        </div>
+        <div className="userChat">
+            <img src="" alt="" />
+            <div className="userChatInfo"></div>
+            <span>Jimmy</span>
+        </div>
+    </div>
   )
 }
 
