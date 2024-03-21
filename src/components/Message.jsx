3662cab5af6d125b77548ccd0b145673a46209cd
Luke Ward <lbw4459@rit.edu>
2024-03-21 09:49:07 -0400

    finished design i think

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='message'>
+    <div className='message owner'>
         <div className="messageInfo">
             <img src="" alt="" />
             <span>now</span>
