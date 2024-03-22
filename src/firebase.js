8346ff2561d4d1be5d803bde3345597c89224d32
Luke Ward <lbw4459@rit.edu>
2024-03-21 21:39:32 -0400

    no build dir

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
+import { API_KEY } from "../key"
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: API_KEY,
   authDomain: "lukes-chat-706f4.firebaseapp.com",
   projectId: "lukes-chat-706f4",
   storageBucket: "lukes-chat-706f4.appspot.com",
